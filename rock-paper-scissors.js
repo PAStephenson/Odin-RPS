50768fa94be4ad65f0fd4cb46d141ab7368d553a
PAStephenson <82534032+PAStephenson@users.noreply.github.com>
2021-12-12 00:52:13 +0000

    Added playRound() to play a single game of rock, paper, scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,12 +1,40 @@
 function computerPlay() {
-	const moves = ["Rock", "Paper", "Scissors"];
+	const moves = ["ROCK", "PAPER", "SCISSORS"];
 	let move = Math.floor(Math.random() * moves.length);
 	return moves[move];
 }
 
 function playerPlay() {
 	let playerMove = prompt("Enter Rock, Paper or Scissors: ");
-	return playerMove
+	return playerMove;
+}
+
+function playRound(player, computer) {
+	const winMessage = "You Win!"
+	const loseMessage = "You Lose!"
+	const drawMessage = "Draw!"
+
+	if (player.toUpperCase() == computer) {
+		return drawMessage;
+	} else if (player.toUpperCase() == "ROCK") {
+		if (computer == "SCISSORS") {
+			return winMessage;
+		} else if (computer == "PAPER") {
+			return loseMessage;
+		}
+	} else if (player.toUpperCase() == "PAPER") {
+		if (computer == "SCISSORS") {
+			return winMessage;
+		} else if (computer == "ROCK") {
+			return loseMessage;
+		}
+	} else if (player.toUpperCase() == "SCISSORS") {
+		if (computer == "PAPER") {
+			return winMessage;
+		} else if (computer == "ROCK") {
+			return loseMessage;
+		}
+	}
 }
 
 const playerSelection = playerPlay();
@@ -14,3 +42,7 @@ const computerSelection = computerPlay();
 
 console.log(playerSelection);
 console.log(computerSelection);
+
+let result = playRound(playerSelection, computerSelection);
+
+console.log(result);
