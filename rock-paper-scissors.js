b1aec2deec698d04f17bc7448f98c786b52ba1a2
PAStephenson <82534032+PAStephenson@users.noreply.github.com>
2021-12-30 22:11:18 +0000

    Added web browser ui

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,68 +1,151 @@
+// User Interface variables
+const btnAttack = document.querySelector('#attack');
+const btnDisarm = document.querySelector('#disarm');
+const btnBlock = document.querySelector('#block');
+const winnerMessage = document.querySelector('#winnerMessage');
+const moveMessage = document.querySelector('#moveMessage');
+const playerMove = document.querySelector('#playerMove');
+const computerMove = document.querySelector('#computerMove');
+const playerTotal = document.querySelector('#playerTotal');
+const computerTotal = document.querySelector('#computerTotal');
+
+btnAttack.addEventListener('click', () => {game('ATTACK')});
+btnDisarm.addEventListener('click', () => {game('DISARM')});
+btnBlock.addEventListener('click', () => {game('BLOCK')});
+
+// Model overall winner message variables
+const modelContainer = document.querySelector('#modelContainer');
+const modelMessage = document.querySelector('#modelMessage');
+const btnModel = document.querySelector('#btnModel');
+
+btnModel.addEventListener('click', () => {resetGame()});
+
+// Game variables
+let playerScore = 0;
+let computerScore = 0;
+const numberOfGames = 5;
+
+function updateMove(player, computer) {
+	switch (player) {
+		case 'ATTACK':
+			playerMove.textContent = '⚡️';
+			break;
+		case 'DISARM':
+			playerMove.textContent = '🌀';
+			break;
+		case 'BLOCK':
+			playerMove.textContent = '💠';
+			break;
+	}
+
+	switch (computer) {
+		case 'ATTACK':
+			computerMove.textContent = '⚡️';
+			break;
+		case 'DISARM':
+			computerMove.textContent = '🌀';
+			break;
+		case 'BLOCK':
+			computerMove.textContent = '💠';
+			break;
+		}
+}
+
+function updateScore() {
+	playerTotal.textContent = `Player: ${playerScore}`;
+	computerTotal.textContent = `Computer: ${computerScore}`;
+}
+
+function updateMessage(player, computer, result) {
+	if (result == "DRAW") {
+		winnerMessage.textContent = "Its a Draw"
+		// Puts player move into title case (first letter capital, rest lower)
+		playerTitle = player.charAt(0) + player.slice(1).toLowerCase();
+		moveMessage.textContent = `${playerTitle} draws with ${computer.toLowerCase()}`
+	} else if (result == "PLAYER WIN") {
+		winnerMessage.textContent = "You win!"
+		// Puts player move into title case (first letter capital, rest lower)
+		playerTitle = player.charAt(0) + player.slice(1).toLowerCase();
+		moveMessage.textContent = `${playerTitle} beats ${computer.toLowerCase()}`
+	} else if (result == "COMPUTER WIN") {
+		winnerMessage.textContent = 'You lose!'
+		// Puts computer move into title case (first letter capital, rest lower)
+		computerTitle = computer.charAt(0) + computer.slice(1).toLowerCase();
+		moveMessage.textContent = `${computerTitle} beats ${player.toLowerCase()}`
+	}
+}
+
+function endGame(result) {
+	modelContainer.classList.add('show');   
+
+	switch (result) {
+		case "PLAYER WIN":	
+			modelMessage.textContent = "You won!";
+			break;
+		case "COMPUTER WIN":
+			modelMessage.textContent = "You lost!";
+			break;
+	}
+}
+
+function resetGame() {
+	modelContainer.classList.remove('show'); 
+
+	playerScore = 0;
+	computerScore = 0;
+	updateScore();
+
+	playerMove.textContent = '?';
+	computerMove.textContent = '?';
+	winnerMessage.textContent = "Choose Your Spell";
+	message.textContent = 'Who will strike first?';
+}
+
 function computerPlay() {
-	const moves = ["ROCK", "PAPER", "SCISSORS"];
-	let move = Math.floor(Math.random() * moves.length);
+	const moves = ["ATTACK", "DISARM", "BLOCK"];
+	const move = Math.floor(Math.random() * moves.length);
 	return moves[move];
 }
 
-function playerPlay() {
-	let playerMove = prompt("Enter Rock, Paper or Scissors: ");
-	return playerMove;
-}
-
 function playRound(player, computer) {
-	if (player.toUpperCase() == computer) {
+	if (player == computer) {
 		return "DRAW";
-	} else if (player.toUpperCase() == "ROCK") {
-		if (computer == "SCISSORS") {
+	} else if (player == "ATTACK") {
+		if (computer == "DISARM") {
 			return "PLAYER WIN";
-		} else if (computer == "PAPER") {
+		} else if (computer == "BLOCK") {
 			return "COMPUTER WIN";
 		}
-	} else if (player.toUpperCase() == "PAPER") {
-		if (computer == "ROCK") {
+	} else if (player == "DISARM") {
+		if (computer == "BLOCK") {
 			return "PLAYER WIN";
-		} else if (computer == "SCISSORS") {
+		} else if (computer == "ATTACK") {
 			return "COMPUTER WIN";
 		}
-	} else if (player.toUpperCase() == "SCISSORS") {
-		if (computer == "PAPER") {
+	} else if (player == "BLOCK") {
+		if (computer == "ATTACK") {
 			return "PLAYER WIN";
-		} else if (computer == "ROCK") {
+		} else if (computer == "DISARM") {
 			return "COMPUTER WIN";
 		}
 	}
 }
 
-function game() {
-	const numberOfGames = 5;
-	let playerScore = 0;
-	let computerScore = 0;
+function game(player) {
+	const computer = computerPlay();
+	const result = playRound(player, computer);
 
-	for (let i = 0; i < numberOfGames; i++) {
-		const playerSelection = playerPlay();
-		const computerSelection = computerPlay();
-
-		console.log(`You selected ${playerSelection.toUpperCase()}`);
-		console.log(`The computer selected ${computerSelection}`);
-
-		const result = playRound(playerSelection, computerSelection);
-
-		if (result == "PLAYER WIN") {
-			playerScore += 1;
-		} else {
-			computerScore += 1;
-		}
-
-		console.log(result);
+	if (result == "PLAYER WIN") {
+		playerScore += 1;
+	} else if (result == "COMPUTER WIN") {
+		computerScore += 1;
 	}
 
-	if (playerScore == computerScore) {
-		console.log(`After ${numberOfGames} games, its a draw.`);
-	} else if (playerScore > computerScore) {
-		console.log(`After ${numberOfGames} games, you are the overall winner!`);
-	} else {
-		console.log(`After ${numberOfGames} games, the computer is the overall winner!`);
+	updateMessage(player, computer, result);
+	updateMove(player, computer);
+	updateScore();
+
+	if (playerScore == numberOfGames || computerScore == numberOfGames) {
+		endGame(result);
 	}
 }
-
-game();
